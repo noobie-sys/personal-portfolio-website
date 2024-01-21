5b2a88b4a7b4aa27c0f7cba6906d71e15b6bee02
Aman Gupta <amangupta954055@gmail.com>
2024-01-21 15:31:05 +0530

    Improve some code

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 		<section className="flex flex-col justify-center gap-4 py-8 md:py-10">
 			<div className="text-[8em] w-3/4 text-slate-400 ">
 				<span>Personal </span>
-				<span className="text-[10rem] text-violet-300 leading-6 uppercase">Portfolio</span>
+				<span className="text-[10rem] text-violet-300 leading-6 uppercase font-mono">Portfolio</span>
 			</div>
 			
 		</section>
