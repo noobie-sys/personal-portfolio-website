dba768d228b070a93a17d5ce6714e671585a1850
Aman Gupta <amangupta954055@gmail.com>
2024-01-24 00:42:00 +0530

    clean some code in about.tsx

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import "@/components/About/about.css";
-import { Button } from "@nextui-org/button";
-import { FaArrowRight } from "react-icons/fa";
-import Image from "next/image";
 import AboutShort from "./About_Short/AboutShort";
 import AboutLong from "./About_long/AboutLong";
 type Props = {};
