44988be4d64bcfe03b3cc9a5a00b1be046af48a6
Aman Gupta <amangupta954055@gmail.com>
2024-01-24 19:15:34 +0530

    add button in about long section

--- components/About/About_long/AboutLong.tsx
+++ components/About/About_long/AboutLong.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import '@/components/About/about.css'
 import Image from 'next/image'
 import code from '@/public/code2.png'
+import { Button } from '@nextui-org/button'
+import { FaArrowRight } from 'react-icons/fa'
 
 type Props = {}
 
@@ -14,6 +16,15 @@ const AboutLong = (props: Props) => {
         </div>
        <div className='desc_about_me'>
         <h1>Hello, I'm Aman Gupta, and I have a deep passion for web development, software engineering, and all things tech-related. I constantly strive to stay abreast of the latest technologies, always eager for new learning experiences. Beyond coding, I enjoy <strong >traveling</strong>, indulging in <strong>Coffee</strong>, and sometimes <strong>Dancing and Singing</strong>. I appreciate your time in reading this, and I'd like to express my gratitude. If you're looking for a <strong>dedicated professional</strong>, consider hiring me. If you're not convinced yet, feel free to <strong>scroll down</strong> for more about what I bring to the table. </h1>
+
+        <Button
+              color="default"
+              variant="bordered"
+              endContent={<FaArrowRight />}
+              className="px-10 py-5 rounded-[4px] "
+            >
+              Contact me 
+            </Button>
        </div>
 
     </div>
