ee9800ca39081475e88d1965f08c33aae6352fde
Aman Gupta <amangupta954055@gmail.com>
2024-02-05 23:54:36 +0530

    clean some code

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import Contact from "@/components/Contacts/Contact";
 export default function Home() {
   return (
     <section className="mobile-responsive w-full h-full flex flex-col justify-center relative z-40">
-      <h1 className="responsive-text hidden  text-[4rem] text-slate-400 ">Sorry! for not reaching to you but this is not responsive for mobile</h1>
       <Hero />
       <Skill />
       <About />
