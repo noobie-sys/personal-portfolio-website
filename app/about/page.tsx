463266fd6a0057b38938965c0b6784f76a377e88
Aman Gupta <amangupta954055@gmail.com>
2024-02-05 17:17:54 +0530

    complete project

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,20 +1,35 @@
 import Cards from "@/ui/card";
 import React from "react";
+import EventHub from "@/public/event.webp";
+import redditCLone from "@/public/reddit-clone.webp";
+import portfoliSite from "@/public/portfolio.jpeg";
 
 type Details = {
-  name : string,
-  desc : string,
-  image : string,
-  links : string
-}
+  name: string;
+  desc: string;
+  image: string;
+  links: string;
+};
 
 const About = () => {
-  const workDetails : Details = [
+  const workDetails = [
     {
-      name: "Project 1",
-      desc: "",
-      image: "",
-      links: "",
+      name: "Event Hub",
+      desc: "Event Hub is where you can host and edit events, it is made up with next.js, typescript , javascript , firebase, atom fot state management and many other library for styles",
+      image: EventHub,
+      links: "https://event-hub-3fv5.vercel.app",
+    },
+    {
+      name: "Reddit Clone",
+      desc: "Reddit is huge platform for hangout, so i thought i have to make it so i did it. It was made up with next.js , atom for state management, tailwinds, typescript and javascript and many other style framerwork",
+      image: redditCLone,
+      links: "https://reddit-clone-app-eight.vercel.app",
+    },
+    {
+      name: "Previous Portfolio site",
+      desc: "My first ever portfolio site where i post my two work and it made up with tailwind css and next.js",
+      image: portfoliSite,
+      links: "https://aman-gupta.vercel.app",
     },
   ];
 
@@ -25,13 +40,15 @@ const About = () => {
       </h1>
 
       <div className="flex flex-wrap gap-4 justify-center items-center ">
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
+        {workDetails.map((el, i) => (
+          <Cards
+            desc={el.desc}
+            image={el.image}
+            link={el.links}
+            name={el.name}
+            key={i}
+          />
+        ))}
       </div>
     </div>
   );
--- ui/card.tsx
+++ ui/card.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { AnchorHTMLAttributes } from "react";
 import Face from "../public/space.jpg";
-import Image, { ImageProps } from "next/image";
+import Image, { ImageProps, StaticImageData } from "next/image";
 import "./card.css";
 import Link from "next/link";
 import FramerMagneticEffect from "@/utils/framerMagneticEffect";
@@ -10,30 +10,29 @@ import { siteConfig } from "@/config/site";
 type Props = {
   name : string,
   desc : string ,
-  image : ImageProps,
+  image : StaticImageData,
   link : string
 }
 
 
-const Cards = () => {
+const Cards = ({name , desc , image , link} : Props) => {
   return (
     <FramerMagneticEffect >
      
       <div  className="card-div max-w-sm  ">
         
 
-        <Link href={siteConfig.links.github}>
-          <Image className="rounded-t-lg object-cover" src={Face} alt="" />
+        <Link href={link} className="image-card">
+          <Image className="rounded-t-lg object-cover" src={image} alt="" />
         </Link>
         <div className="card-detail">
           <div>
             <h5 className="mb-2 ml-3 text-2xl font-bold tracking-tight">
-              Project 1
+              {name}
             </h5>
           </div>
           <p className="mb-14 font-normal  ">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+            {desc}
           </p>
         </div>
       </div>
