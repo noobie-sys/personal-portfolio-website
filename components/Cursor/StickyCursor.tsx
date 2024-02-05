2b0b95af2a60b9eea0e8bc7fdd4303cca8a6414e
Aman Gupta <amangupta954055@gmail.com>
2024-02-06 01:36:30 +0530

    fix some bugs

--- components/Cursor/StickyCursor.tsx
+++ components/Cursor/StickyCursor.tsx
@@ -1,42 +1,34 @@
 "use client";
-import React, {
-
-  useEffect,
-} from "react";
+import React, { useEffect } from "react";
 import "./stickyCursor.css";
 import { motion, useMotionValue, useSpring } from "framer-motion";
 
 const StickyCursor = () => {
-  
-  const cursorSize =  40;
+  const cursorSize = 40;
 
   const mouse = {
     x: useMotionValue(0),
     y: useMotionValue(0),
   };
 
-  
-
   const smoothOption = { damping: 20, stiffness: 300, mass: 0.5 };
   const smoothMouse = {
     x: useSpring(mouse.x, smoothOption),
     y: useSpring(mouse.y, smoothOption),
   };
+  const manageMouseMove = (e: MouseEvent) => {
+    // console.log(e.clientX)
+    const { pageX, pageY } = e;
+    mouse.x.set(pageX - cursorSize / 2);
+    mouse.y.set(pageY - cursorSize / 2);
+  };
   useEffect(() => {
-    const manageMouseMove = (e: MouseEvent) => {
-      // console.log(e.clientX)
-      const { pageX, pageY } = e;
-      mouse.x.set(pageX - cursorSize / 2);
-      mouse.y.set(pageY - cursorSize / 2);
-    };
-
     window.addEventListener("mousemove", manageMouseMove);
-    
+
     return () => {
       window.removeEventListener("mousemove", manageMouseMove);
-      
     };
-  }, []);
+  }, [manageMouseMove]);
   return (
     <motion.div
       className="cursor"
@@ -45,7 +37,6 @@ const StickyCursor = () => {
         top: smoothMouse.y,
       }}
       id="cursor"
-     
     ></motion.div>
   );
 };
