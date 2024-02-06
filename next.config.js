1f74c0f386c6953bab9142e3c497758fedc92e0b
Aman Gupta <amangupta954055@gmail.com>
2024-02-06 14:18:46 +0530

    fix bugs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
 
-module.exports = nextConfig
+module.exports = {
+    // other configurations...
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        // Adjust the ignore pattern as needed
+        config.resolve.alias['micromatch'] = require.resolve('micromatch');
+      }
+  
+      return config;
+    },
+  };
+  
