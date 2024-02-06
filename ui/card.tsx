ec750189965bba1b4a11b38551d8ef46ab963189
Aman Gupta <amangupta954055@gmail.com>
2024-02-06 12:32:39 +0530

    cleaning some code

--- ui/card.tsx
+++ ui/card.tsx
@@ -17,64 +17,54 @@ type Props = {
 const Cards = ({ name, desc, image, link }: Props) => {
   const mouseRef = useRef<HTMLDivElement>(null);
 
-  // useEffect(() => {
-  //   const cursor = document.querySelector<HTMLElement>("#cursor");
-  //   const mouseRefElement = mouseRef.current;
+  useEffect(() => {
+    const cursor = document.querySelector<HTMLElement>("#cursor");
+    const mouseRefElement = mouseRef.current;
 
-  //   if (mouseRefElement) {
-  //     mouseRefElement.addEventListener("mouseover", () => {
-  //       if (cursor) {
-  //         cursor.style.width = "100px";
-  //         cursor.style.height = "100px";
-  //         cursor.innerHTML = "<h1>Click Image</h1>";
-  //         cursor.style.transform = "translate(-50% ,-50%)";
-  //       }
-  //     });
-  //     mouseRefElement.addEventListener("mouseleave", () => {
-  //       if (cursor) {
-  //         cursor.style.width = "40px";
-  //         cursor.style.height = "40px";
-  //         cursor.innerHTML = "";
-  //         cursor.style.transform = "translate(0,0)";
-  //       }
-  //     });
-  //   }
-  //   // return () => {
-  //   //   if (mouseRefElement) {
-  //   //     mouseRefElement.removeEventListener("mouseover", () => {
-  //   //       if (cursor) {
-  //   //         cursor.style.width = "100px";
-  //   //         cursor.style.height = "100px";
-  //   //         cursor.innerHTML = "<h1>Click Image</h1>";
-  //   //         cursor.style.transform = "translate(-50% ,-50%)";
-  //   //       }
-  //   //     });
-  //   //     mouseRefElement.removeEventListener("mouseleave", () => {
-  //   //       if (cursor) {
-  //   //         cursor.style.width = "40px";
-  //   //         cursor.style.height = "40px";
-  //   //         cursor.innerHTML = "";
-  //   //         cursor.style.transform = "translate(0,0)";
-  //   //       }
-  //   //     });
-  //   //   }
-  //   // };
-  // }, []);
+    const mouseMove = () => {
+      if (cursor) {
+        cursor.style.width = "100px";
+        cursor.style.height = "100px";
+        cursor.innerHTML = "<h1>Click Image</h1>";
+        cursor.style.transform = "translate(-50% ,-50%)";
+      }
+    }
+
+    const mouseLeave = () => {
+      if (cursor) {
+        cursor.style.width = "40px";
+        cursor.style.height = "40px";
+        cursor.innerHTML = "";
+        cursor.style.transform = "translate(0,0)";
+      }
+    }
+
+    if (mouseRefElement) {
+      mouseRefElement.addEventListener("mouseover", mouseMove);
+      mouseRefElement.addEventListener("mouseleave", mouseLeave);
+    }
+    return () => {
+      if (mouseRefElement) {
+        mouseRefElement.removeEventListener("mouseover", mouseMove);
+        mouseRefElement.removeEventListener("mouseleave", mouseLeave);
+      }
+    };
+  }, []);
 
   return (
-      <div className="card-div max-w-sm  " ref={mouseRef}>
-        <Link href={link} className="image-card">
-          <Image className="rounded-t-lg object-cover" src={image} alt="" />
-        </Link>
-        <div className="card-detail">
-          <div>
-            <h5 className="mb-2 ml-3 text-2xl font-bold tracking-tight">
-              {name}
-            </h5>
-          </div>
-          <p className="mb-14 font-normal  ">{desc}</p>
+    <div className="card-div max-w-sm  " ref={mouseRef}>
+      <Link href={link} className="image-card">
+        <Image className="rounded-t-lg object-cover" src={image} alt="" />
+      </Link>
+      <div className="card-detail">
+        <div>
+          <h5 className="mb-2 ml-3 text-2xl font-bold tracking-tight">
+            {name}
+          </h5>
         </div>
+        <p className="mb-14 font-normal  ">{desc}</p>
       </div>
+    </div>
   );
 };
 
