d8dfe866b82ea3dc4d44192212f3db97d119f9d4
Aman Gupta <amangupta954055@gmail.com>
2024-02-06 13:56:18 +0530

    delete unwanted part

--- utils/framerMagneticEffect.tsx
+++ utils/framerMagneticEffect.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { EventHandler, useEffect, useRef, useState } from "react";
+import React, {  useRef, useState } from "react";
 import { motion } from "framer-motion";
 
 
